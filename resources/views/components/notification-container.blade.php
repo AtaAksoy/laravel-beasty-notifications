6928d74f30729a0aa17a224462c108cc9050fee4
Ata AKSOY <ataaaksoy@gmail.com>
2024-12-02 12:42:16 +0300

    container empty array fix

--- resources/views/components/notification-container.blade.php
+++ resources/views/components/notification-container.blade.php
@@ -1,21 +1,35 @@
-<script>
-    const toasts = new Toasts({
-        offsetX: 20, // 20px
-        offsetY: 20, // 20px
-        gap: 20, // The gap size in pixels between toasts
-        width: 350, // 300px
-        timing: 'ease', // See list of available CSS transition timings
-        duration: '.5s', // Transition duration
-        dimOld: false, // Dim old notifications while the newest notification stays highlighted
-        position: 'top-right' // top-left | top-center | top-right | bottom-left | bottom-center | bottom-right
-    });
+<div>
+    <link rel="stylesheet" href="{{ asset('ataaaksoy/laravel-beasty-notifications/css/notification.css') }}">
+    <script src="{{ asset('ataaaksoy/laravel-beasty-notifications/js/notification.js') }}"></script>
 
-    @foreach (session()->get('laravel-beasty-notifications') as $notif)
-        toasts.push({
-            title: 'Error Toast',
-            content: 'My notification description.',
-            style: 'error',
-            dismissAfter: '3s'
+    <script>
+        const toasts = new Toasts({
+            offsetX: 20, // 20px
+            offsetY: 20, // 20px
+            gap: 20, // The gap size in pixels between toasts
+            width: 350, // 300px
+            timing: 'ease', // See list of available CSS transition timings
+            duration: '.5s', // Transition duration
+            dimOld: false, // Dim old notifications while the newest notification stays highlighted
+            position: 'top-right' // top-left | top-center | top-right | bottom-left | bottom-center | bottom-right
         });
-    @endforeach
-</script>
+    
+        @foreach (session()->get('laravel-beasty-notifications', []) as $notif)
+            toasts.push({
+                title: 'Error Toast',
+                content: 'My notification description.',
+                style: 'error',
+                dismissAfter: '3s'
+            });
+
+        @endforeach
+
+
+        @if (session()->has('laravel-beasty-notifications'))
+            @php
+                session()->forget('laravel-beasty-notifications');
+            @endphp
+        @endif
+    </script>
+</div>
+
